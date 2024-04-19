817f99d8575ee18d28f960ba585398c95a47a4a3
Alexis Sanchez <alexis@debian-net.sa>
2024-04-19 04:36:21 -0300

    se cambio la logica del update

--- api/controllers/update.py
+++ api/controllers/update.py
@@ -7,44 +7,25 @@ def update_restaurant(restaurant_data):
         conn_db = conectionDb()
         cursor = conn_db.conn.cursor()
 
-        # Comprobamos si restaurant_data tiene solo un campo a actualizar
-        if len(restaurant_data) == 2 and 'id' in restaurant_data and 'update_field' in restaurant_data:
-            # Construimos la sentencia SQL de actualización para actualizar un solo campo
+        # Comprobamos si restaurant_data tiene los campos necesarios
+        if 'id' in restaurant_data and 'campo' in restaurant_data and 'valor' in restaurant_data:
+            # Construimos la sentencia SQL de actualización
             update_query = f'''
             UPDATE restaurants.public.restaurants
-            SET {restaurant_data['update_field']} = %s
+            SET {restaurant_data['campo']} = %s
             WHERE id = %s
             '''
             # Ejecutamos la sentencia SQL de actualización
-            cursor.execute(update_query, (restaurant_data['value'], restaurant_data['id']))
+            cursor.execute(update_query, (restaurant_data['valor'], restaurant_data['id']))
+
+            # Confirmamos la transacción
+            conn_db.conn.commit()
+
+            # Retornamos un mensaje de éxito
+            return jsonify({'message': 'Registro actualizado exitosamente'})
         else:
-            # Construimos la sentencia SQL de actualización para actualizar todos los campos
-            update_query = '''
-            UPDATE restaurants.public.restaurants
-            SET rating = %s, name = %s, site = %s, email = %s, phone = %s,
-                street = %s, city = %s, state = %s, lat = %s, lng = %s
-            WHERE id = %s
-            '''
-            # Ejecutamos la sentencia SQL de actualización
-            cursor.execute(update_query, (
-                restaurant_data['rating'],
-                restaurant_data['name'],
-                restaurant_data['site'],
-                restaurant_data['email'],
-                restaurant_data['phone'],
-                restaurant_data['street'],
-                restaurant_data['city'],
-                restaurant_data['state'],
-                restaurant_data['lat'],
-                restaurant_data['lng'],
-                restaurant_data['id']
-            ))
-
-        # Confirmamos la transacción
-        conn_db.conn.commit()
-
-        # Retornamos un mensaje de éxito
-        return jsonify({'message': 'Registro actualizado exitosamente'})
+            # Retornamos un mensaje de error si falta algún campo
+            return jsonify({'error': 'Faltan campos obligatorios en los datos proporcionados'})
 
     except Exception as e:
         # Manejamos cualquier excepción que pueda ocurrir durante la actualización
